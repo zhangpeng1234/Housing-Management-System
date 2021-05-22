475e2320ad0dd08119a8190d1a798e526793ee82
woniu <2601805807@qq.com>
2021-05-22 16:05:48 +0800

    Add files via upload

---
+++ RentProgram/src/com/pengzhang/rent/App/HouseRentApp.java
@@ -0,0 +1,14 @@
+package com.pengzhang.rent.App;
+
+import com.pengzhang.rent.view.HouseView;
+
+/**
+ * @author zhang
+ * @version 1.0
+ */
+public class HouseRentApp {
+    public static void main(String[] args) {
+        HouseView houseView=new HouseView();
+        houseView.mainMenu();
+    }
+}
